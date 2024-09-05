3aa61abc81aa6c98f9cc6ef52e40ae97abbcb968
Naiko2004 <nicolasantiagomez4@gmail.com>
2024-09-05 10:08:12 -0300

    Arregle algunos errores de anochey ya termine lo de crear alumnos, inscribirlos a una carrera y a las materias del plan

--- acad/acad_plantilla.sql
+++ acad/acad_plantilla.sql
@@ -177,48 +177,53 @@ VALUES(7, 2016, 1);
 INSERT INTO Materia (id_materia, nombre)
 VALUES(3, 'Tecnicas Digitales');
 INSERT INTO Plan_materia (id_plan_materia, id_plan_de_estudio, id_materia, id_jefe_catedra, año)
-VALUES(3, 3, 3, 3, 2018);
+VALUES(3, 3, 3, 1, 2018);
 INSERT INTO Plan_materia (id_plan_materia, id_plan_de_estudio, id_materia, id_jefe_catedra, año)
-VALUES(4, 4, 3, 3, 2024);
+VALUES(4, 4, 3, 1, 2024);
 INSERT INTO Materia (id_materia, nombre)
 VALUES(4, 'Ingenieria de Software');
 INSERT INTO Plan_materia (id_plan_materia, id_plan_de_estudio, id_materia, id_jefe_catedra, año)
-VALUES(5, 3, 4, 3, 2014);
+VALUES(5, 3, 4, 1, 2014);
 INSERT INTO Plan_materia (id_plan_materia, id_plan_de_estudio, id_materia, id_jefe_catedra, año)
-VALUES(6, 4, 4, 3, 2024);
+VALUES(6, 4, 4, 1, 2024);
 INSERT INTO Materia (id_materia, nombre)
 VALUES(5, 'Electronica Fisica');
 INSERT INTO Plan_materia (id_plan_materia, id_plan_de_estudio, id_materia, id_jefe_catedra, año)
-VALUES(7, 3, 5, 3, 2004);
+VALUES(7, 3, 5, 1, 2004);
 INSERT INTO Plan_materia (id_plan_materia, id_plan_de_estudio, id_materia, id_jefe_catedra, año)
-VALUES(8, 4, 5, 3, 2024);
+VALUES(8, 4, 5, 1, 2024);
 
 INSERT INTO Materia (id_materia, nombre)
 VALUES(6, 'Quimica Organica');
 INSERT INTO Plan_materia (id_plan_materia, id_plan_de_estudio, id_materia, id_jefe_catedra, año)
-VALUES(9, 5, 6, 3, 2020);
+VALUES(9, 5, 6, 1, 2020);
 INSERT INTO Plan_materia (id_plan_materia, id_plan_de_estudio, id_materia, id_jefe_catedra, año)
-VALUES(10, 6, 6, 3, 2024);
+VALUES(10, 6, 6, 1, 2024);
 INSERT INTO Materia (id_materia, nombre)
 VALUES(7, 'Economia');
 INSERT INTO Plan_materia (id_plan_materia, id_plan_de_estudio, id_materia, id_jefe_catedra, año)
-VALUES(11, 5, 7, 3, 2021);
+VALUES(11, 5, 7, 1, 2021);
 INSERT INTO Plan_materia (id_plan_materia, id_plan_de_estudio, id_materia, id_jefe_catedra, año)
-VALUES(12, 6, 7, 3, 2023);
+VALUES(12, 6, 7, 1, 2023);
 INSERT INTO Materia (id_materia, nombre)
 VALUES(8, 'Pensamiento Teologico');
 INSERT INTO Plan_materia (id_plan_materia, id_plan_de_estudio, id_materia, id_jefe_catedra, año)
-VALUES(13, 5, 8, 3, 2021);
+VALUES(13, 5, 8, 1, 2021);
 INSERT INTO Plan_materia (id_plan_materia, id_plan_de_estudio, id_materia, id_jefe_catedra, año)
-VALUES(14, 6, 8, 3, 2024);
+VALUES(14, 6, 8, 1, 2024);
 
 INSERT INTO Materia (id_materia, nombre)
 VALUES(9, 'Arquitectura de Software II');
 INSERT INTO Plan_materia (id_plan_materia, id_plan_de_estudio, id_materia, id_jefe_catedra, año)
-VALUES(15, 1, 9, 3, 2017);
+VALUES(15, 7, 9, 1, 2017);
 INSERT INTO Plan_materia (id_plan_materia, id_plan_de_estudio, id_materia, id_jefe_catedra, año)
-VALUES(16, 7, 9, 3, 2024);
-
+VALUES(16, 1, 9, 1, 2024);
+INSERT INTO Plan_materia (id_plan_materia, id_plan_de_estudio, id_materia, id_jefe_catedra, año)
+VALUES(17, 7, 2, 1, 2017);
+INSERT INTO Plan_materia (id_plan_materia, id_plan_de_estudio, id_materia, id_jefe_catedra, año)
+VALUES(18, 1, 2, 1, 2024);
+INSERT INTO Plan_materia (id_plan_materia, id_plan_de_estudio, id_materia, id_jefe_catedra, año)
+VALUES(19, 7, 1, 1, 2017);
 
 
 // CREAR ALUMNOS, INSCRIBIRLOS A UN PLAN DE UNA CARRERA E INSCRIBIRLOS A TODAS LAS MATERIAS DEL PLAN.
@@ -246,107 +251,107 @@ VALUES(5, 7, 2024, 1);
 INSERT INTO Comision (id_comision, id_cursada, nombre, id_aula)
 VALUES(5, 5, 'Comision A', 1);
 INSERT INTO Alumno_comision (id_comision, id_alumno, fecha_inscripcion, estado_inscripcion, fecha_estado)
-VALUES(5, 3, '2022-01-12', '2022-02-12', 'Cursando');
+VALUES(5, 3, '2022-01-12', 'Cursando', '2022-02-12');
 
 INSERT INTO Persona (id_persona, nombre, apellido, dni, fecha_nacimiento, direccion, mail, telefono)
-VALUES(5, '', '', , '', '', '', '');
+VALUES(5, 'Alejandra', 'Guerrero', 3344112, '2003-04-01', 'Calle Realeza 329', 'alejandra.guerrero@mail.com', '376 4433112');
 INSERT INTO Alumno (id_alumno, id_persona)
 VALUES(4, 5);
 INSERT INTO Alumno_plan (id_alumno, id_plan_de_estudio, fecha_matriculacion, condicion)
-VALUES(4, , '', '');
+VALUES(4, 4, '2024-01-01', 'Cursando');
 INSERT INTO Cursada (id_cursada, id_plan_materia, año_cursada, cuatrimestre)
-VALUES(6, , , );
+VALUES(6, 4, 2024, 2);
 INSERT INTO Comision (id_comision, id_cursada, nombre, id_aula)
-VALUES(6, 6, '', );
+VALUES(6, 6, 'Comision C', 2);
 INSERT INTO Alumno_comision (id_comision, id_alumno, fecha_inscripcion, estado_inscripcion, fecha_estado)
-VALUES(6, 4, '', '', '');
+VALUES(6, 4, '2024-07-02', 'Inscripto', '2024-07-07');
 INSERT INTO Cursada (id_cursada, id_plan_materia, año_cursada, cuatrimestre)
-VALUES(7, , , );
+VALUES(7, 6, 2024, 2);
 INSERT INTO Comision (id_comision, id_cursada, nombre, id_aula)
-VALUES(7, 7, '', );
+VALUES(7, 7, 'Comision B2', 1);
 INSERT INTO Alumno_comision (id_comision, id_alumno, fecha_inscripcion, estado_inscripcion, fecha_estado)
-VALUES(7, 4, '', '', '');
+VALUES(7, 4, '2024-07-02', 'Inscripto', '2024-07-07');
 INSERT INTO Cursada (id_cursada, id_plan_materia, año_cursada, cuatrimestre)
-VALUES(8, , , );
+VALUES(8, 8, 2024, 1);
 INSERT INTO Comision (id_comision, id_cursada, nombre, id_aula)
-VALUES(8, 8, '', );
+VALUES(8, 8, 'Comision A', 2);
 INSERT INTO Alumno_comision (id_comision, id_alumno, fecha_inscripcion, estado_inscripcion, fecha_estado)
-VALUES(8, 4, '', '', '');
+VALUES(8, 4, '2024-03-01', 'Inscripto', '2024-03-01');
 
 INSERT INTO Persona (id_persona, nombre, apellido, dni, fecha_nacimiento, direccion, mail, telefono)
-VALUES(6, '', '', , '', '', '', '');
+VALUES(6, 'Fernando', 'Altamirano', 441232, '2003-01-04', 'Calle Ratrue 31', 'fernando.altamirano@mail.com', '351 5423312');
 INSERT INTO Alumno (id_alumno, id_persona)
 VALUES(5, 6);
 INSERT INTO Alumno_plan (id_alumno, id_plan_de_estudio, fecha_matriculacion, condicion)
-VALUES(5, , '', '');
+VALUES(5, 5, '2020-01-01', 'Cursando');
 INSERT INTO Cursada (id_cursada, id_plan_materia, año_cursada, cuatrimestre)
-VALUES(9, , , );
+VALUES(9, 9, 2020, 1);
 INSERT INTO Comision (id_comision, id_cursada, nombre, id_aula)
-VALUES(9, 9, '', );
+VALUES(9, 9, 'Comision C2', 2);
 INSERT INTO Alumno_comision (id_comision, id_alumno, fecha_inscripcion, estado_inscripcion, fecha_estado)
-VALUES(9, 5, '', '', '');
+VALUES(9, 5, '2020-03-03', 'Inscripto', '2020-03-03');
 INSERT INTO Cursada (id_cursada, id_plan_materia, año_cursada, cuatrimestre)
-VALUES(10, , , );
+VALUES(10, 11, 2020, 2);
 INSERT INTO Comision (id_comision, id_cursada, nombre, id_aula)
-VALUES(10, 10, '', );
+VALUES(10, 10, 'Comision A1', 1);
 INSERT INTO Alumno_comision (id_comision, id_alumno, fecha_inscripcion, estado_inscripcion, fecha_estado)
-VALUES(10, 5, '', '', '');
+VALUES(10, 5, '2020-03-04', 'Cancelada', '2020-04-03');
 INSERT INTO Cursada (id_cursada, id_plan_materia, año_cursada, cuatrimestre)
-VALUES(11, , , );
+VALUES(11, 13, 2020, 1);
 INSERT INTO Comision (id_comision, id_cursada, nombre, id_aula)
-VALUES(11, 11, '', );
+VALUES(11, 11, 'Comision B1', 2);
 INSERT INTO Alumno_comision (id_comision, id_alumno, fecha_inscripcion, estado_inscripcion, fecha_estado)
-VALUES(11, 5, '', '', '');
+VALUES(11, 5, '2020-03-02', 'Inscripto', '2020-03-03');
 
 INSERT INTO Persona (id_persona, nombre, apellido, dni, fecha_nacimiento, direccion, mail, telefono)
-VALUES(7, '', '', , '', '', '', '');
+VALUES(7, 'Hector', 'Martinez', 43332112, '2002-10-10', 'Calle Salvador 444', 'hector.martinez@mail.com', '351 4332211');
 INSERT INTO Alumno (id_alumno, id_persona)
 VALUES(6, 7);
 INSERT INTO Alumno_plan (id_alumno, id_plan_de_estudio, fecha_matriculacion, condicion)
-VALUES(6, , '', '');
+VALUES(6, 6, '2024-01-01', 'Cursando');
 INSERT INTO Cursada (id_cursada, id_plan_materia, año_cursada, cuatrimestre)
-VALUES(12, , , );
+VALUES(12, 10, 2024, 1);
 INSERT INTO Comision (id_comision, id_cursada, nombre, id_aula)
-VALUES(12, 12, '', );
+VALUES(12, 12, 'Comision A', 2);
 INSERT INTO Alumno_comision (id_comision, id_alumno, fecha_inscripcion, estado_inscripcion, fecha_estado)
-VALUES(12, 6, '', '', '');
+VALUES(12, 6, '2024-01-02', 'Inscripto', '2024-01-03');
 INSERT INTO Cursada (id_cursada, id_plan_materia, año_cursada, cuatrimestre)
-VALUES(13, , , );
+VALUES(13, 12, 2024, 1);
 INSERT INTO Comision (id_comision, id_cursada, nombre, id_aula)
-VALUES(13, 13, '', );
+VALUES(13, 13, 'Comision C1', 1);
 INSERT INTO Alumno_comision (id_comision, id_alumno, fecha_inscripcion, estado_inscripcion, fecha_estado)
-VALUES(13, 6, '', '', '');
+VALUES(13, 6, '2024-01-01', 'Inscripto', '2024-01-02');
 INSERT INTO Cursada (id_cursada, id_plan_materia, año_cursada, cuatrimestre)
-VALUES(14, , , );
+VALUES(14, 14, 2024, 1);
 INSERT INTO Comision (id_comision, id_cursada, nombre, id_aula)
-VALUES(14, 14, '', );
+VALUES(14, 14, 'Comision B3', 1);
 INSERT INTO Alumno_comision (id_comision, id_alumno, fecha_inscripcion, estado_inscripcion, fecha_estado)
-VALUES(14, 6, '', '', '');
+VALUES(14, 6, '2024-01-01', 'Inscripto', '2024-01-03');
 
 INSERT INTO Persona (id_persona, nombre, apellido, dni, fecha_nacimiento, direccion, mail, telefono)
-VALUES(8, '', '', , '', '', '', '');
+VALUES(8, 'Agustin', 'Leonardo', 43123321, '2003-02-10', 'Calle San Martin 542', 'agustin.leonardo@mail.com', '351 5632321');
 INSERT INTO Alumno (id_alumno, id_persona)
 VALUES(7, 8);
 INSERT INTO Alumno_plan (id_alumno, id_plan_de_estudio, fecha_matriculacion, condicion)
-VALUES(7, , '', '');
+VALUES(7, 7, '2018-03-03', 'Cursando');
 INSERT INTO Cursada (id_cursada, id_plan_materia, año_cursada, cuatrimestre)
-VALUES(15, , , );
+VALUES(15, 15, 2020, 2);
 INSERT INTO Comision (id_comision, id_cursada, nombre, id_aula)
-VALUES(15, 15, '', );
+VALUES(15, 15, 'Comision F5', 1);
 INSERT INTO Alumno_comision (id_comision, id_alumno, fecha_inscripcion, estado_inscripcion, fecha_estado)
-VALUES(15, 7, '', '', '');
+VALUES(15, 7, '2020-01-01', 'Inscripto', '2020-01-01');
 INSERT INTO Cursada (id_cursada, id_plan_materia, año_cursada, cuatrimestre)
-VALUES(16, , , );
+VALUES(16, 17, 2020, 2);
 INSERT INTO Comision (id_comision, id_cursada, nombre, id_aula)
-VALUES(16, 16, '', );
+VALUES(16, 16, 'Comision G9', 1);
 INSERT INTO Alumno_comision (id_comision, id_alumno, fecha_inscripcion, estado_inscripcion, fecha_estado)
-VALUES(16, 7, '', '', '');
+VALUES(16, 7, '2020-02-02', 'Inscripto', '2020-02-03');
 INSERT INTO Cursada (id_cursada, id_plan_materia, año_cursada, cuatrimestre)
-VALUES(17, , , );
+VALUES(17, 19, 2020, 1);
 INSERT INTO Comision (id_comision, id_cursada, nombre, id_aula)
-VALUES(17, 17, '', );
+VALUES(17, 17, 'Comision L8', 1);
 INSERT INTO Alumno_comision (id_comision, id_alumno, fecha_inscripcion, estado_inscripcion, fecha_estado)
-VALUES(17, 7, '', '', '');
+VALUES(17, 7, '2020-02-03', 'Inscripto', '2020-02-03');
 
 
 
