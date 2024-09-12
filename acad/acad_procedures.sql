8e2425de713a1b16cc93dd75c6e31c47118c4373
Naiko2004 <nicolasantiagomez4@gmail.com>
2024-09-12 14:22:06 -0300

    a

--- acad/acad_procedures.sql
+++ acad/acad_procedures.sql
@@ -1,5 +1,15 @@
 USE acad;
 
+DROP PROCEDURE contarAlumnos;
+DROP PROCEDURE cargarCarrera;
+DROP PROCEDURE cargarPlanDeEstudio;
+DROP PROCEDURE cargarPlanMateria;
+DROP PROCEDURE matricularAlumno;
+DROP PROCEDURE inscripcionCursada;
+DROP PROCEDURE inscripcionExamen;
+DROP PROCEDURE cargarParcial;
+DROP PROCEDURE cargarNota;
+
 delimiter //
 CREATE PROCEDURE contarAlumnos()
 BEGIN
@@ -485,7 +495,7 @@ END//
 delimiter ;
 
 -- Registro de Parciales;
--- call cargarParcial(4343112, 'Tecnicas Digitales', 'Ingeniería en Computación', 4)
+-- call cargarParcial(4343112, 'Tecnicas Digitales', 'Ingeniería en Computación', 4);
 
 delimiter //
 CREATE PROCEDURE cargarParcial(IN pDniAlumno int, IN pNombreMateria varchar(50), IN pNombreCarrera varchar(50), IN pNota int)
@@ -632,4 +642,85 @@ proc_label:BEGIN
 
 	
 END//
+delimiter ;
+
+-- CargarNotaFinal;
+-- call cargarNota(12345678, 'Base de Datos II', -4);
+
+
+delimiter //
+
+CREATE PROCEDURE cargarNota(IN pDniAlumno int, IN pMateria varchar(50), IN pNota int)
+proc_label: BEGIN
+
+  DECLARE idAlumno INT DEFAULT 0;
+  DECLARE idMateria INT DEFAULT 0;
+  DECLARE isInscripto INT DEFAULT 0;
+  DECLARE hasNota INT DEFAULT 0;
+
+  IF pNota < 1 OR pNota > 10 THEN
+    SELECT 'La nota no es válida';
+    LEAVE proc_label;
+  END IF;
+
+  SELECT a.id_alumno INTO idAlumno
+  FROM Alumno a JOIN Persona p on a.id_persona = p.id_persona
+  WHERE p.dni = pDniAlumno;
+
+
+  IF idAlumno = 0 THEN
+    SELECT 'El DNI ingresado no pertenece a un Alumno registrado';
+    LEAVE proc_label;
+  END IF;
+
+
+  SELECT ma.id_materia INTO idMateria
+  FROM Materia ma
+  WHERE ma.nombre = pMateria;
+
+
+  IF idMateria = 0 THEN
+    SELECT 'No existe esa materia';
+    LEAVE proc_label;
+  END IF;
+
+
+  SELECT ie.id_inscripcion INTO isInscripto
+  FROM Alumno a 
+  JOIN Inscripcion_examen ie ON a.id_alumno = ie.id_alumno
+  JOIN Turno_examen te on ie.id_turno_examen = te.id_turno_examen
+  WHERE a.id_alumno = idAlumno AND te.id_materia = idMateria
+  ORDER BY ie.fecha_inscripcion DESC
+  LIMIT 1;
+
+  IF isInscripto = 0 THEN
+    SELECT 'El alumno no está inscripto en el examen';
+    LEAVE proc_label;
+  END IF;
+
+
+  SELECT ie.nota INTO hasNota
+  FROM Alumno a 
+  JOIN Inscripcion_examen ie ON a.id_alumno = ie.id_alumno
+  JOIN Turno_examen te on ie.id_turno_examen = te.id_turno_examen
+  WHERE a.id_alumno = idAlumno AND te.id_materia = idMateria
+  ORDER BY ie.fecha_inscripcion DESC
+  LIMIT 1;
+
+  IF hasNota > 0 THEN
+    SELECT 'El alumno ya tiene nota';
+    LEAVE proc_label;
+  END IF;
+
+
+  UPDATE Inscripcion_examen
+  SET nota = pNota
+  WHERE id_inscripcion = isInscripto;
+
+  SELECT ie.nota 
+  FROM Inscripcion_examen ie 
+  WHERE ie.id_inscripcion = isInscripto;
+
+END//
+
 delimiter ;
